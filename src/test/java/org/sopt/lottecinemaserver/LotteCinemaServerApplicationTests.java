02c72a1dff6bc570c9cc3d042f6cf5ae2aef3e12
HongGit <soojam@konkuk.ac.kr>
2024-11-20 21:30:23 +0900

    setting: 프로젝트 생성

---
+++ src/test/java/org/sopt/lottecinemaserver/LotteCinemaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.lottecinemaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteCinemaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
