13b3c86ebc49a2fffd5731502ed404226bab3a4e
HongGit <soojam@konkuk.ac.kr>
2024-11-28 20:30:45 +0900

    feat: CORS 처리 설정파일 추가

---
+++ src/main/java/org/sopt/lottecinemaserver/global/config/WebConfig.java
@@ -0,0 +1,19 @@
+package org.sopt.lottecinemaserver.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173",
+                        "https://lottecinema.shop", "https://www.seonyak.com", "https://server.lottecinema.shop")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
