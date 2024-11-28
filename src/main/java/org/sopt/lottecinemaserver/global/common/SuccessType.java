22604ef22535d2f974a961ad5964257174173fac
HongGit <soojam@konkuk.ac.kr>
2024-11-28 20:31:13 +0900

    fix: 예매된 좌석 조회 문구 변경

--- src/main/java/org/sopt/lottecinemaserver/global/common/SuccessType.java
+++ src/main/java/org/sopt/lottecinemaserver/global/common/SuccessType.java
@@ -9,7 +9,7 @@ public enum SuccessType {
     PROCESS_SUCCESS(HttpStatus.OK, "OK"),
     GET_ONLY_MOVIE_LIST(HttpStatus.OK, "단독 영화가 성공적으로 조회되었습니다"),
     GET_POPULAR_MOVIE_LIST(HttpStatus.OK, "인기 영화가 성공적으로 조회되었습니다"),
-    GET_AVAILABLE_SEATS(HttpStatus.OK, "예매 가능 좌석정보가 성공적으로 조회되었습니다"),
+    GET_AVAILABLE_SEATS(HttpStatus.OK, "예매된 좌석정보가 성공적으로 조회되었습니다"),
     POST_RESERVE_SEATS(HttpStatus.OK, "예매에 성공했습니다"),
     GET_SCREEN_TIME_LIST_SUCCESS(HttpStatus.OK, "성공적으로 상영관,상영시간 리스트를 조회했습니다."),
     GET_REGION_LIST_SUCCESS(HttpStatus.OK, "지역 리스트가 성공적으로 조회되었습니다."),
